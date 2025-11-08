9da27a012d97fbd04eafbf675db46232a91fe859
Ritvik Patil <pritvik0@gmail.com>
2025-11-08 19:52:23 +0530

    feat: local postgres db setup

--- config/settings.py
+++ config/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'maintenance_tracker_db'),
+        'USER': os.environ.get('DB_USER', 'main_tracker_user'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', 'localhost'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
